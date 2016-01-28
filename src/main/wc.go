ad8e34d7be2ab504c4571d3c092453b302d6374e
willx8 <willx8@gmail.com>
2016-01-29 02:03:44 +0800

    first commit

--- src/main/wc.go
+++ src/main/wc.go
@@ -1,18 +1,39 @@
 package main
 
 import "os"
+import "log"
 import "fmt"
 import "mapreduce"
 import "container/list"
+import "strconv"
+import "unicode"
+import "strings"
 
 // our simplified version of MapReduce does not supply a
 // key to the Map function, as in the paper; only a value,
 // which is a part of the input file contents
 func Map(value string) *list.List {
+	l := list.New()
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c)
+	}
+	for _, word := range strings.FieldsFunc(value, f) {
+		l.PushBack(mapreduce.KeyValue{word, "1"})
+	}
+	return l
 }
 
-// iterate over list and add values
+// Iterate over list and add values.
 func Reduce(key string, values *list.List) string {
+	count := 0
+	for v := values.Front(); v != nil; v = v.Next() {
+		value, err := strconv.Atoi(v.Value.(string))
+		if err != nil {
+			log.Fatal("Reduce: ", err)
+		}
+		count += value
+	}
+	return strconv.Itoa(count)
 }
 
 // Can be run in 3 ways:
@@ -20,17 +41,17 @@ func Reduce(key string, values *list.List) string {
 // 2) Master (e.g., go run wc.go master x.txt localhost:7777)
 // 3) Worker (e.g., go run wc.go worker localhost:7777 localhost:7778 &)
 func main() {
-  if len(os.Args) != 4 {
-    fmt.Printf("%s: see usage comments in file\n", os.Args[0])
-  } else if os.Args[1] == "master" {
-    if os.Args[3] == "sequential" {
-      mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
-    } else {
-      mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])    
-      // Wait until MR is done
-      <- mr.DoneChannel
-    }
-  } else {
-    mapreduce.RunWorker(os.Args[2], os.Args[3], Map, Reduce, 100)
-  }
+	if len(os.Args) != 4 {
+		fmt.Printf("%s: see usage comments in file\n", os.Args[0])
+	} else if os.Args[1] == "master" {
+		if os.Args[3] == "sequential" {
+			mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
+		} else {
+			mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])
+			// Wait until MR is done
+			<-mr.DoneChannel
+		}
+	} else {
+		mapreduce.RunWorker(os.Args[2], os.Args[3], Map, Reduce, 100)
+	}
 }
--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -1,32 +1,134 @@
 package mapreduce
+
 import "container/list"
 import "fmt"
+import "log"
+import "time"
+import "strconv"
+import "sync"
 
 type WorkerInfo struct {
-  address string
-  // You can add definitions here.
+	address string
+	// You can add definitions here.
+	busy      bool
+	bad       bool
+	failTimes int
 }
-
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
 func (mr *MapReduce) KillWorkers() *list.List {
-  l := list.New()
-  for _, w := range mr.Workers {
-    DPrintf("DoWork: shutdown %s\n", w.address)
-    args := &ShutdownArgs{}
-    var reply ShutdownReply;
-    ok := call(w.address, "Worker.Shutdown", args, &reply)
-    if ok == false {
-      fmt.Printf("DoWork: RPC %s shutdown error\n", w.address)
-    } else {
-      l.PushBack(reply.Njobs)
-    }
-  }
-  return l
+	l := list.New()
+	for _, w := range mr.Workers {
+		log.Printf("DoWork: shutdown %s\n", w.address)
+		args := &ShutdownArgs{}
+		var reply ShutdownReply
+		ok := call(w.address, "Worker.Shutdown", args, &reply)
+		if ok == false {
+			fmt.Printf("DoWork: RPC %s shutdown error\n", w.address)
+		} else {
+			l.PushBack(reply.Njobs)
+		}
+	}
+	return l
+}
+
+func SingleWorker(wk *WorkerInfo, m *sync.Mutex, jobList *list.List, done chan bool, inProgress *int) {
+	for wk.bad == false {
+		m.Lock()
+		if jobList.Len() == 0 {
+			m.Unlock()
+			break
+		}
+		job := jobList.Front()
+		jobList.Remove(job)
+		(*inProgress)++ // keep tracking number of in-progress jobs.
+		m.Unlock()
+
+		var reply DoJobReply
+		ok := call(wk.address, "Worker.DoJob", job.Value.(*DoJobArgs), &reply)
+		if ok == false {
+			wk.failTimes++
+			if wk.failTimes >= 10 {
+				wk.bad = true
+			}
+			fmt.Printf("DoWork: RPC %s Do%s error\n", wk.address, job.Value.(*DoJobArgs).Operation)
+			// re-assign the job
+			m.Lock()
+			jobList.PushFront(job.Value.(*DoJobArgs))
+			m.Unlock()
+		}
+
+		m.Lock()
+		(*inProgress)--
+		if jobList.Len() == 0 {
+			if *inProgress == 0 {
+				defer func() {
+					fmt.Printf("Job Done.\n")
+					done <- true
+				}()
+			}
+			m.Unlock()
+			break
+		}
+		m.Unlock()
+	}
 }
 
 func (mr *MapReduce) RunMaster() *list.List {
-  // Your code here
-  return mr.KillWorkers()
+	timeout := time.After(3000 * time.Millisecond)
+	flag := true
+	finish := make(chan int)
+	mapMutex := &sync.Mutex{}
+	reduceMutex := &sync.Mutex{}
+	wkMutex := &sync.Mutex{}
+	inProgressMap := 0
+	inProgressReduce := 0
+
+	go func() {
+		<-mr.DoneMapChan
+		mr.doneMap = true
+		wkMutex.Lock()
+		for _, wk := range mr.Workers {
+			go SingleWorker(wk, reduceMutex, mr.JobListReduce, mr.DoneReduceChan, &inProgressReduce)
+		}
+		wkMutex.Unlock()
+	}()
+
+	go func() {
+		for i := 0; flag; i += 1 {
+			select {
+			case <-mr.DoneReduceChan:
+				flag = false
+				finish <- 1
+			case <-timeout:
+				flag = false
+			case address := <-mr.registerChannel:
+				// keep listening for worker registration while doing job.
+				timeout = time.After(3000 * time.Millisecond)
+
+				workername := "worker" + strconv.Itoa(i)
+				wkinfo := new(WorkerInfo)
+				wkinfo.address = address
+				wkMutex.Lock()
+				mr.Workers[workername] = wkinfo
+				wkMutex.Unlock()
+
+				log.Printf("worker %d:%s", i, address)
+				if mr.doneMap == false {
+					go SingleWorker(wkinfo, mapMutex, mr.JobListMap, mr.DoneMapChan, &inProgressMap)
+				} else {
+					go SingleWorker(wkinfo, reduceMutex, mr.JobListReduce, mr.DoneReduceChan, &inProgressReduce)
+				}
+			}
+		}
+	}()
+
+	select {
+	// if two <-mr.DoneReduceChan exists at same time, (the other at line 101)
+	// finish (line 103) will be emitted to ensure the close.
+	case <-finish:
+	case <-mr.DoneReduceChan:
+	}
+	return mr.KillWorkers()
 }
